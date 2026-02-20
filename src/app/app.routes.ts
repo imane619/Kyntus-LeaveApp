b615af5a1086b96409158fe29a94b2f7937f95cc
IMANE OUARY <IMANE.OUARY@kpi.lan>
2026-02-20 15:51:30 +0100

    Initial commit: Angular frontend and .NET backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // chemin vide, on affiche le Dashboard
+  { path: '', component: DashboardComponent },
+  
+  // Quand on clique sur "Demander un congé"
+  { path: 'demander', component: DashboardComponent },
+  
+  // une adresse qui n'existe pas,  revenir à l'accueil
+  { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; 
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  // C'est ICI qu'on insère les modules : dans la liste "imports"
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive, 
+    CommonModule
+  ], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
